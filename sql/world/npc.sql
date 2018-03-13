7edac5231fb5f18798436c730d03f88b1b049eb6
talamortis <talamortis@gmail.com>
2018-03-13 21:29:36 +0000

    ADDED NPC FOR EASY USE

---
+++ sql/world/npc.sql
@@ -0,0 +1,30 @@
+-- ######################################################--
+--	CODEBOX - 90024
+-- ######################################################--
+SET
+@Entry 		:= 90024,
+@Model 		:= 16804, -- Elven Jeweler
+@Name 		:= "Retdream",
+@Title 		:= "Keeper of Codes",
+@Icon 		:= "Buy",
+@GossipMenu := 0,
+@MinLevel 	:= 80,
+@MaxLevel 	:= 80,
+@Faction 	:= 35,
+@NPCFlag 	:= 1,
+@Scale		:= 1.0,
+@Rank		:= 0,
+@Type 		:= 7,
+@TypeFlags 	:= 138936390,
+@FlagsExtra := 2,
+@AIName		:= "SmartAI",
+@Script 	:= "reward_shop";
+
+-- NPC
+DELETE FROM creature_template WHERE entry = @Entry;
+INSERT INTO creature_template (entry, modelid1, name, subname, IconName, gossip_menu_id, minlevel, maxlevel, faction, npcflag, speed_walk, speed_run, scale, rank, unit_class, unit_flags, type, type_flags, InhabitType, RegenHealth, flags_extra, AiName, ScriptName) VALUES
+(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, 1, 1.14286, @Scale, @Rank, 1, 2, @Type, @TypeFlags, 3, 1, @FlagsExtra, @AIName, @Script);
+
+-- NPC Text
+DELETE FROM `npc_text` WHERE `ID`=@Entry;
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry, 'Greetings $N. Do you have a loot code to redeem?');
